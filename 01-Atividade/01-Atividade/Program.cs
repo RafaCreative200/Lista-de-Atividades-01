5d5e4d72fc76231805ff4b4e3ca8f88c532af09d
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-03 10:45:46 -0300

    Atividade 1

---
+++ 01-Atividade/01-Atividade/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine($"O valor de A é: {a}");
+        Console.WriteLine($"O valor de B é: {b}");
+
+    }
+}
