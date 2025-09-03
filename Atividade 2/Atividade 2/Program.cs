e96bfb45fc791df13734e61bc55810e51c4c98a8
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-03 10:51:32 -0300

    Atv 2

---
+++ Atividade 2/Atividade 2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int antecessor = numero - 1;
+
+Console.WriteLine($"O antecessor de {numero} é {antecessor}");
