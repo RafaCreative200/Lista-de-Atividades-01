05ae16569f5484ff1c6a182b3d060ea1b091534b
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 09:13:14 -0300

    13- Atividade

---
+++ 13- Atividade/13- Atividade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro valor: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo valor: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o terceiro valor: ");
+        int c = int.Parse(Console.ReadLine());
+
+      
+        int[] valores = { a, b, c };
+        
+        Array.Sort(valores);
+        Console.WriteLine("Valores em ordem crescente:");
+        foreach (int v in valores)
+        {
+            Console.Write(v + " ");
+        }
+    }
+}
