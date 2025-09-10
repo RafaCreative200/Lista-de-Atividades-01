81489311876a910e832f60e5b73cdb3614162044
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:28:58 -0300

    10- Atividade

---
+++ 10- Atividade/10- Atividade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a nota da primeira avaliação: ");
+        double nota1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota da segunda avaliação: ");
+        double nota2 = double.Parse(Console.ReadLine());
+
+        double media = (nota1 + nota2) / 2;
+
+        Console.WriteLine($"\nMédia do aluno: {media:F2}");
+
+        if (media >= 6.0)
+        {
+            Console.WriteLine("Aluno APROVADO!");
+        }
+        else
+        {
+            Console.WriteLine("Aluno REPROVADO!");
+        }
+    }
+}
