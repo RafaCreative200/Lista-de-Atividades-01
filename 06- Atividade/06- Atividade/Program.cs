4de45c5ecbcd1ccff721c2f9be39ae102c78ad2b
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:57:27 -0300

    06- Atividade

---
+++ 06- Atividade/06- Atividade/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o salário mensal atual: R$ ");
+        double salarioAtual = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o percentual de reajuste (%): ");
+        double percentual = double.Parse(Console.ReadLine());
+
+        double novoSalario = salarioAtual + (salarioAtual * percentual / 100);
+
+        Console.WriteLine($"\nO novo salário é: R$ {novoSalario:F2}");
+    }
+}
