b7d77fbf3ce87990d3cfd30dfe074151f34bb337
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 11:05:28 -0300

    Extra - 03

---
+++ Extra Atividade 03/Extra Atividade 03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de A: ");
+        double A = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite o valor de B: ");
+        double B = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite o valor de C: ");
+        double C = Convert.ToDouble(Console.ReadLine());
+        double delta = Math.Pow(B, 2) - 4 * A * C;
+        Console.WriteLine($"\nO valor de Delta é: {delta}");
+    }
+}
