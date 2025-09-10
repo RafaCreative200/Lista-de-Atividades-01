dd6f6993f453fdfbd413fc67ca05c47adfa4187e
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:37:27 -0300

    12- Atividade

---
+++ 12- Atividade/12- Atividade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro valor: ");
+        double valor1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo valor: ");
+        double valor2 = double.Parse(Console.ReadLine());
+
+        if (valor1 > valor2)
+        {
+            Console.WriteLine($"\nO maior valor é: {valor1}");
+        }
+        else
+        {
+            Console.WriteLine($"\nO maior valor é: {valor2}");
+        }
+    }
+}
