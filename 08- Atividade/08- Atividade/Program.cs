83d20c1dd64249378686471c733e8533070ec12a
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:07:16 -0300

    08- Atividade

---
+++ 08- Atividade/08- Atividade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor (não pode ser zero): ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double valor))
+        {
+            if (valor == 0)
+            {
+                Console.WriteLine("Valor inválido! Zero não é permitido.");
+            }
+            else if (valor > 0)
+            {
+                Console.WriteLine("O valor é positivo.");
+            }
+            else
+            {
+                Console.WriteLine("O valor é negativo.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida! Digite um número válido.");
+        }
+    }
+}
