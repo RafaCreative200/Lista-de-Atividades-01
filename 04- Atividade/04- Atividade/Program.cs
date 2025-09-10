ffd095daa06b14097e9cd745386297ea13de14a4
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:45:15 -0300

    04- Atividade

---
+++ 04- Atividade/04- Atividade/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a idade em anos: ");
+        int anos = int.Parse(Console.ReadLine());
+
+        int idadeEmDias = anos * 365;
+
+        Console.WriteLine($"\nA idade aproximada em dias é: {idadeEmDias} dias");
+    }
+}
