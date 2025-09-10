05a8684eaf1738ccc360a1e7bc9187cce7283d49
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:03:02 -0300

    Atividade 07

---
+++ 07- Atividade/07- Atividade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int valor))
+        {
+            if (valor > 10)
+            {
+                Console.WriteLine("É maior que 10!");
+            }
+            else
+            {
+                Console.WriteLine("Não é maior que 10!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido! Digite um número inteiro.");
+        }
+    }
+}
