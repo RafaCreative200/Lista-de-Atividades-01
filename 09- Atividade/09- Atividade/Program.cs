dfd996a59d4f1335da0bfec0cfeee4b48dbafe22
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:19:16 -0300

    09- Atividade

---
+++ 09- Atividade/09- Atividade/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a quantidade de maçãs compradas: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        double preco;
+
+        if (quantidade < 12)
+        {
+            preco = 1.30;
+        }
+        else
+        {
+            preco = 1.00;
+        }
+
+        double total = quantidade * preco;
+
+        Console.WriteLine($"O custo total da compra é: R$ {total:F2}");
+    }
+}
