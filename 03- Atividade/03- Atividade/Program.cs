40258d99b8e03a04e1cc2aab1b5603c87b57c4f4
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-10 08:42:41 -0300

    03- Atividade

---
+++ 03- Atividade/03- Atividade/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor da base do retângulo: ");
+        double baseRetangulo = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor da altura do retângulo: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double area = baseRetangulo * altura;
+
+        Console.WriteLine($"\nA área do retângulo é: {area:F2}");
+    }
+}
