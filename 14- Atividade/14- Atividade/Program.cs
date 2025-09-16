7832d6ab7278f8b792654edbc52e65c6262db032
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-16 08:16:18 -0300

    14- Atividade

---
+++ 14- Atividade/14- Atividade/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
