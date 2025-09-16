dc2e76da092e888f13d30284930716e5697a0e33
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-16 08:21:00 -0300

    15- Atividade

---
+++ 15- Atividade/15- Atividade/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números entre 100 e 2000 que deixam resto 5 quando divididos por 11:");
+
+        for (int i = 100; i <= 2000; i++)
+        {
+            if (i % 11 == 5)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
