425e24e144fdab1ec710bbad6b3a35c9dcd08629
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-16 07:48:56 -0300

    Atividade 01 - While

---
+++ Atividade01/Atividade01/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num, i, resultado, valor;
+Console.WriteLine("Digite um numero para a multiplicação");
+num = int.Parse(Console.ReadLine());
+Console.WriteLine("Até que numero você quer que seja multiplicado?");
+valor = int.Parse(Console.ReadLine());
+i = 0;
+while(i <=valor )
+{
+    resultado = i * num;
+    Console.WriteLine($"{num} X {i} = {resultado} ");
+    i++;
+}
+
---
+++ Extra Atividade 04/Extra Atividade 04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o preço do produto: ");
+        double preco = Convert.ToDouble(Console.ReadLine());
+
+        double desconto = preco * 0.05;
+        double precoPromocional = preco - desconto;
+
+        Console.WriteLine($"O preço promocional com 5% de desconto é: R$ {precoPromocional}");
+    }
+}
