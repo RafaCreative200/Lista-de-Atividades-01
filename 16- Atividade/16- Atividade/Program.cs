a945380074b1f5d4571126963e128b3e65282668
Rafael Spindola <rafaelmatutandoideias@gmail.com>
2025-09-16 08:52:32 -0300

    16- Atividade

---
+++ 16- Atividade/16- Atividade/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 0)
+        {
+            Console.WriteLine("Fatorial não existe para números negativos.");
+        }
+        else
+        {
+            long resultado = Fatorial(n);
+            Console.WriteLine($"{n}! = {resultado}");
+        }
+    }
+
+    static long Fatorial(int n)
+    {
+        if (n == 0) return 1; // definição: 0! = 1
+
+        long fat = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            fat *= i;
+        }
+        return fat;
+    }
+}
+
